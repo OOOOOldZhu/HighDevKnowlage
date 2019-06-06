e869ab3a94bad305306114e325f7b1feeacbdc63
z <zhuhaiwen@microduino.cc>
2019-06-06 16:27:12 +0800

    组合控件

---
+++ viewstudy/src/main/java/com/z/viewstudy/view/SimpleLayout.java
@@ -0,0 +1,52 @@
+package com.z.viewstudy.view;
+
+/*
+ * ：Created by z on 2019/6/5
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SimpleLayout extends ViewGroup {
+
+    public SimpleLayout(Context context) {
+        super(context);
+    }
+
+    public SimpleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        this.setBackgroundColor(Color.YELLOW);
+        if (getChildCount() > 0) {
+            View child = getChildAt(0);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (getChildCount() > 0) {
+            View childView = getChildAt(0);
+            childView.layout(0, 0, childView.getMeasuredWidth(), childView.getMeasuredHeight());
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+
+}
